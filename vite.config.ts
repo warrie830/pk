5e74ab6ff16f756967d96dbc8b928768c187fdf6
liqx <liqx@zhejianglab.com>
2025-08-12 00:45:54 +0800

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // 加载环境变量
-  const env = loadEnv(mode, process.cwd(), "");
-
-  // 获取API基础URL，如果没有设置则使用默认值
-  const apiBaseUrl = "http://localhost:5001";
-
+export default defineConfig(({}) => {
   return {
     plugins: [react()],
     css: {
